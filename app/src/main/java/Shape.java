db794f09ef06bbd4b3329c3a3fcd335a2bdf5787
Zaviyar Zahid <zaviyarzahid34@gmail.com>
2026-02-17 13:57:59 +0500

    Circleee

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,4 +1,14 @@
 public class Shape {
     int x;
     int y;
+
+    public static class Rect extends Shape {
+
+
+        public Rect(int x, int y, int radius) {
+            super(x, y);
+
+            //Commitnewn
+        }
+    }
 }
