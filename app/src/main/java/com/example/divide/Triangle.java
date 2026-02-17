365db73fcbb7c44e1ad2e80a15f4591f1b69d44f
MuhammadKhan79 <26100138@lums.edu.pk>
2026-02-17 16:06:32 +0500

    Fixed triangle class

--- app/src/main/java/com/example/divide/Triangle.java
+++ app/src/main/java/com/example/divide/Triangle.java
@@ -1,7 +1,6 @@
 public class Triangle extends Shape {
     private int base;
     private int height;
-    private String color = "blue";
 
     public Triangle(int x, int y, int base, int height) {
         this.x = x;
